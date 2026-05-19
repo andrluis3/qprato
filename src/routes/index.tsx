d6f3a9c52ad5cdd8c5f805e41fdc0b9e826581ad
Andre <andreluis3@hotmail.com>
2026-05-19 19:38:45 -0300

    Inclusão de "restaurantes", "bares", "opções

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,7 +26,7 @@ function HomePage() {
   const [q, setQ] = useState("");
   const { city, status, retry } = useUserCity();
 
-  const rotatingWords = ["pratos", "temperos", "aromas", "petiscos", "sabores"];
+  const rotatingWords = ["pratos", "temperos", "aromas", "petiscos", "sabores", "restaurantes", "bares", "opções"];
   const [wordIndex, setWordIndex] = useState(0);
   useEffect(() => {
     const id = setInterval(() => {
